405b88e23f1ab2d7237e2bcab81392b3c075de95
Peter Eckersley <pde@eff.org>
2011-11-09 17:21:34 -0800

    Working towards correct git and directory management for crawls

--- crawl.py
+++ crawl.py
@@ -4,6 +4,10 @@ import subprocess
 import threading
 import Queue
 import random
+import git
+import time
+import os,os.path,shutil
+import sys
 
 # wget -E -H -e robots=off --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)"  -kp https://www.facebook.com/terms.php
 # TODO(dta) use multiprocessing module instead?
@@ -14,13 +18,16 @@ GLOBAL_UAS = ["Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko
 
 QUEUE = Queue.Queue()
 
+operating_path = os.path.dirname(sys.argv[0])
 
 class TOSCrawler(threading.Thread):
     def __init__(self, queue):
         threading.Thread.__init__(self)
         self.queue = queue
-    
+
     def process(self, data):
+
+        # 0. Determine parameters for this crawl
         url = data['url']
         if 'UAs' in data:
             UAs = data['UAs']
@@ -32,23 +39,22 @@ class TOSCrawler(threading.Thread):
         assert "sitename" in data, "Every rule needs a sitename"
         sitename = data["sitename"]
         assert "docname" in data, "Every rule needs a sitename"
-        sitename = data["docname"]
+        docname = data["docname"]
 
-        # figure out what branch we're on
-        # if dirty, git stash
-        # cd crawl/sitename/docname
-        # rm -rf .
-        # wget output goes here
-        # git commit .
-        # git checkout original branch
-        # if was dirty, git stash pop
-
+        # 1. Prepare a directory for the crawl results
+        target = os.path.join(operating_path,"..","crawls",sitename,docname)
+        if os.path.isdir(target):
+            # rm -rf the previous crawl state
+            shutils.rmtree(target)
+        os.path.mkdirs(target)
+        os.path.chdir(target)
 
         # TODO: do wget lookup
         print "Crawling %s\n" % url
         # todo add 
         args = ['wget', '-H', '-p', '-e', 'robots=off', '-o', '%s_wget.log' % url, '-U', random.choice(UAs), url]
         subprocess.call(args)
+        os.path.chdir(operating_path)
         # TODO(dta): rename directory tree, manipulate files
 
     def run(self):
@@ -66,6 +72,11 @@ def ReadFormat(file_to_read):
 def main():
     # 1. get results
     results = [{'url': 'google.com', 'xpath':'path'}, {'url':'facebook.com/terms.php'}]
+
+    # 1.b. make a git branch to work in
+    branchname = "crawl-" + time.strftime("%Y-%m-%d-%H-%M-%S")
+    gitrepo = git.Repo("..")
+    gitrepo.branches[branchname]
 
     # 2. spawn thread pool
     for i in xrange(POOL_SIZE):
