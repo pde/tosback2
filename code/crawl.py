d8aaf556f75a65057e0347915e3c355f16723e55
Dan Auerbach <dan@EFFs-MacBook-Pro-2.local>
2011-11-10 01:08:05 -0800

    take out threading, some general cleanup

--- code/crawl.py
+++ code/crawl.py
@@ -1,91 +1,97 @@
 #!/usr/bin/env python
 
 import subprocess
-import threading
-import Queue
 import random
+import git
+import time
+import os,os.path,shutil
+import sys
 
-# wget -E -H -e robots=off --user-agent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)"  -kp https://www.facebook.com/terms.php
-# TODO(dta) use multiprocessing module instead?
-# TODO(dta) set flag -r l1
+# TODO(dta) use multiprocessing module
+# at some point in the future
 
-POOL_SIZE = 10
 GLOBAL_UAS = ["Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)"]
+OPERATING_PATH = os.path.dirname(sys.argv[0])
 
-QUEUE = Queue.Queue()
+class TOSCrawler(object):
+    """Class to process xml files sequentially"""
+    def __init__(self, xml_file_name=None):
+        if xml_file_name:
+            self.read(xml_file_name)
 
+    def read(self, file_name):
+        """Parses XML file."""
+        # set self.data
+        # TODO for testing: refactor to unit test
+        self.data = {'sitename': 'facebook',
+                     'docname': 'Terms of Service',
+                     'url': 'www.facebook.com/terms.php'}
 
-class TOSCrawler(threading.Thread):
-    def __init__(self, queue):
-        threading.Thread.__init__(self)
-        self.queue = queue
-    
-    def process(self, data):
-        # mandatory
-        assert "sitename" in data, "Every rule needs a sitename"
-        sitename = data["sitename"]
-        assert "docname" in data, "Every rule needs a docname"
-        sitename = data["docname"]
-        assert "url" in data, "Every rule needs a url"
-        url = data["url"]
+    def process(self):
+        # 0. Determine parameters for this crawl
+        assert "sitename" in self.data, "Every rule needs a sitename"
+        sitename = self.data["sitename"]
+        assert "docname" in self.data, "Every rule needs a sitename"
+        docname = self.data["docname"]
+        assert "url" in self.data, "Every rule needs a url"
+        url = self.data["url"]
 
-        # optional
-        if 'UAs' in data:
-            UAs = data['UAs']
+        recurse = "norecurse" not in self.data
+        xpath = "xpath" not in self.data
+
+        if 'UAs' in self.data:
+            UAs = self.data['UAs']
         else:
             UAs = GLOBAL_UAS
-        if 'xpath' in data:
-            xpath = data['xpath']
 
+        # 1. Prepare a directory for the crawl results
+        target = os.path.join(operating_path,"..","crawls",sitename,docname)
+        if os.path.isdir(target):
+            # rm -rf the previous crawl state
+            shutils.rmtree(target)
+        os.path.mkdirs(target)
+        os.path.chdir(target)
 
-        # figure out what branch we're on
-        # if dirty, git stash
-        # cd crawl/sitename/docname
-        # rm -rf .
-        # wget output goes here
-        # git commit .
-        # git checkout original branch
-        # if was dirty, git stash pop
-
-
-        # TODO: do wget lookup
+        # 2. Do wget lookup
         print "Crawling %s\n" % url
-        # todo add 
-        args = ['wget', '-H', '-p', '-e', 'robots=off', '-o', '%s_wget.log' % url, '-U', random.choice(UAs), url]
+        args = [
+            'wget', 
+            # Obtain images, CSS, etc, even from other domains
+            '--page-requisites',
+            '--span-hosts',
+            # Obtain the Terms of Service, even if j.random crawler is blocked
+            '--execute', 'robots=off', 
+            '--user-agent', random.choice(UAs), 
+            # Put results in the right place
+            '--directory-prefix', target,         
+            # '--output-file', '%s_wget.log' % url,   # URLs are not safe filenames
+            '--output-file', 'crawled-terms-of-service.html'
+            # Format things for historical/offline browsing
+            '--convert-links',
+            '--adjust-extension',
+            url]
+        if recurse:
+            args[-1:-1] = ['--recursive', '--level', '1']
         subprocess.call(args)
+        os.path.chdir(operating_path)
         # TODO(dta): rename directory tree, manipulate files
-
-    def run(self):
-        while True:
-            data = self.queue.get()
-            self.process(data)
-            self.queue.task_done()
-
-def ReadFormat(file_to_read):
-    pass
-    # read in from file (create test file for testing)
-    # return results
 
 
 def main():
-    # 1. get results
-    results = [{'url': 'google.com', 'xpath':'path'}, {'url':'facebook.com/terms.php'}]
+    # 1. make a git branch to work in
+    branchname = "crawl-" + time.strftime("%Y-%m-%d-%H-%M-%S")
+    gitrepo = git.Repo("..")
+    gitrepo.branches[branchname]
 
-    # 2. spawn thread pool
-    for i in xrange(POOL_SIZE):
-        t = TOSCrawler(QUEUE)
-        t.daemon = True
-        t.start()
+    # 2. initialize TOSCrawler
+    t = TOSCrawler()
 
-    # 3. populate queue with results
-    for r in results:
-        QUEUE.put(r)
-
-    QUEUE.join()
-
-
-def CheckDiff():
-    pass
+    # 3. Traverse
+    for fi in os.listdir("../rules"):
+        if fi[-4:]!=".xml": continue
+        print "Reading %s" % fi
+        t.read(fi)
+        t.process()
 
 
 main()
