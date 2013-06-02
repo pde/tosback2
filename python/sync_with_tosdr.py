656925193cbbda30efaae9505385b293552c44ba
Jesse Weinstein <jesse@wefu.org>
2013-06-01 18:39:46 -0700

    Add tosback2 format writer to python script, and gitignore

--- python/sync_with_tosdr.py
+++ python/sync_with_tosdr.py
@@ -24,3 +24,22 @@ def tosdr(filename):
 
 def all_items(d,func,ext):
     return [y[0] for y in map(func, [os.path.join(d,x) for x in os.listdir(d) if x.endswith(ext)]) if y]
+
+def missing_from_tosback2(tosdr,tosback2):
+    ks=dict(tosback2).keys()
+    return [x for x in tosdr if x[0] not in ks]
+
+def tosback2_write(data, direct):
+    r=et.Element('sitename',{'name':data[0]})
+    for x in data[1]:
+        et.SubElement(et.SubElement(et.SubElement(\
+            r, "docname", {'name':x[0]}),"url",{'name':x[1]}),"norecurse",{'name':'arbitrary'})
+
+    r.text='\n ';r.tail='\n' #pretty printing
+    for x in r:
+        x.text='\n   ';
+        x.tail=(r[-1]==x and '\n' or '\n ');
+        x[0].text='\n     ';
+        x[0].tail='\n ';
+        x[0][0].tail='\n   '
+    et.ElementTree(r).write(os.path.join(direct, data[0]+'.xml'))
