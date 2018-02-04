8876d595cb40224e9c254337dddfa9fae867f3aa
tosback3 <tosback3@dragon.unhosted.org>
2018-02-04 00:46:41 -0500

    update bash script for new server

--- tosback.sh
+++ tosback.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source /usr/local/rvm/environments/ruby-2.3.1@tb2
+source /home/tosback3/.rvm/environments/ruby-2.3.1@tb2
 LANG=en_US.UTF-8
 
-cd /root/tosback2/rubycode
+cd /home/tosback3/tosback2/rubycode
 bundle exec ruby main.rb 
 git push
